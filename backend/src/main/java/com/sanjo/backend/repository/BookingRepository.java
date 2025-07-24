7bacfdad5a7aad0dbad7c0501bb0c4a2c094b623
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-24 22:13:16 +0530

    Created BookingRepository with methods to fetch bookings by room ID, user ID, and confirmation code

--- backend/src/main/java/com/sanjo/backend/repository/BookingRepository.java
+++ backend/src/main/java/com/sanjo/backend/repository/BookingRepository.java
@@ -1,4 +1,16 @@
 package com.sanjo.backend.repository;
 
-public interface BookingRepository {
+import com.sanjo.backend.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking,Long> {
+
+    List<Booking> findBookingByRoomId(Long roomId);
+    List<Booking> findBookingByBookingConfirmationCode(String confirmationCode);
+    List<Booking> findByUserId(Long userId);
+
 }
