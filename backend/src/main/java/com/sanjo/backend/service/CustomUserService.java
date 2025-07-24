9b3c9bcc79d8264248d07eec016f56c6f6c1b5c9
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-24 23:51:37 +0530

    Implemented CustomUserService to load user details by email with custom exception handling

--- backend/src/main/java/com/sanjo/backend/service/CustomUserService.java
+++ backend/src/main/java/com/sanjo/backend/service/CustomUserService.java
@@ -1,4 +1,25 @@
 package com.sanjo.backend.service;
 
-public class CustomUserService {
+import com.sanjo.backend.exception.OurException;
+import com.sanjo.backend.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserService implements UserDetailsService {
+
+    //Dependency
+    private final UserRepository repository;
+    //Injection
+    public CustomUserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username)
+                .orElseThrow(()-> new OurException("UserName/Email not found"));
+    }
 }
