6290ba8489ea424a2c66bd13373de90358ad7c89
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-24 19:54:58 +0530

    feat: add BookingDTO to structure booking data with nested user and room details

--- backend/src/main/java/com/sanjo/backend/dto/BookingDTO.java
+++ backend/src/main/java/com/sanjo/backend/dto/BookingDTO.java
@@ -1,4 +1,29 @@
 package com.sanjo.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.sanjo.backend.entity.Room;
+import com.sanjo.backend.entity.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BookingDTO {
+
+    private long id;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private int numOfAdult;
+    private int numOfChildren;
+    private int totalNumOfGuest;
+    private String bookingConfirmationCode;
+
+    private UserDTO user;
+
+    private RoomDTO room;
 }
