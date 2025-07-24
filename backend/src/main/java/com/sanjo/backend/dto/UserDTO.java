d13b6354cf429a915e8602a041617ff38779cc63
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-24 19:15:18 +0530

    feat: create UserDTO to expose limited user data without role and password

--- backend/src/main/java/com/sanjo/backend/dto/UserDTO.java
+++ backend/src/main/java/com/sanjo/backend/dto/UserDTO.java
@@ -1,4 +1,24 @@
 package com.sanjo.backend.dto;
 
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.sanjo.backend.entity.Booking;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL) //Don't want to return data with null value
 public class UserDTO {
+
+    private long id;
+    private String email;
+    private String name;
+    private String phoneNumber;
+    private List<Booking> bookings = new ArrayList<>();
 }
+
+//Exposing role and password
