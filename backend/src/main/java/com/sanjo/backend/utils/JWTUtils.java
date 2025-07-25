69d618dc297ff3afca7d1c71e1bb2cd8bf156ad0
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-25 10:58:47 +0530

    Initialized JWTUtils with secret key decoding using @PostConstruct and base64 secret from properties

--- backend/src/main/java/com/sanjo/backend/utils/JWTUtils.java
+++ backend/src/main/java/com/sanjo/backend/utils/JWTUtils.java
@@ -1,4 +1,29 @@
 package com.sanjo.backend.utils;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+
+@Service
 public class JWTUtils {
+
+    private static final long EXPIRATION_TIME = 1000 * 60 * 24 * 7; // 7 days
+    private SecretKey key;
+
+    @Value("${jwt.secret}")
+    private String secretString;
+
+    @PostConstruct
+    public void init() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretString);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
 }
