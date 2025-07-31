a134216817c0592ae1a46749f6473b4abe01bd30
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-31 14:35:48 +0530

    feat: add register endpoint to handle user registration

--- backend/src/main/java/com/sanjo/backend/controller/AuthController.java
+++ backend/src/main/java/com/sanjo/backend/controller/AuthController.java
@@ -1,5 +1,31 @@
 package com.sanjo.backend.controller;
 
 
+import com.sanjo.backend.dto.LoginRequest;
+import com.sanjo.backend.dto.Response;
+import com.sanjo.backend.entity.User;
+import com.sanjo.backend.service.interfac.IUserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    private final IUserService userService;
+
+    public AuthController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody User user) {
+        Response response = userService.register(user);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+
 }
