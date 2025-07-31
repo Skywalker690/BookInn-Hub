d7ff1ca6da88c37e8f7b44f67b6ed66f8b2d30f7
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-31 15:20:13 +0530

    feat: add endpoint to fetch user details by user ID

--- backend/src/main/java/com/sanjo/backend/controller/UserController.java
+++ backend/src/main/java/com/sanjo/backend/controller/UserController.java
@@ -5,6 +5,7 @@ import com.sanjo.backend.service.interfac.IUserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,12 @@ public class UserController {
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<Response> getAllUsers(){
         Response response = userService.getAllUsers();
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @GetMapping("/get-by-id/{userId}")
+    public ResponseEntity<Response> getAllUserById(@PathVariable("userId") String userId){
+        Response response = userService.getUserById(userId);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
