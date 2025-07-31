7717bec1d418a5f78f4397b6314dcd7af4ad834e
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-31 10:24:51 +0530

    feat: implement updateRoom with optional image upload and partial field updates

--- backend/src/main/java/com/sanjo/backend/service/implementation/RoomService.java
+++ backend/src/main/java/com/sanjo/backend/service/implementation/RoomService.java
@@ -100,14 +100,41 @@ public class RoomService implements IRoomService {
         }
         catch (Exception e) {
             response.setStatusCode(500);
-            response.setMessage("Error Occurred While Getting Deleting User "+e.getMessage());
+            response.setMessage("Error Occurred While Getting Deleting Room "+e.getMessage());
         }
         return response;
     }
 
     @Override
     public Response updateRoom(Long roomId, String description, String roomType, BigDecimal roomPrice, MultipartFile photo) {
-        return null;
+        Response response = new Response();
+
+        try {
+            String imageUrl = null;
+            if (photo != null && !photo.isEmpty()) {
+                imageUrl = awsS3Service.saveImageToS3(photo);
+            }
+            Room room = roomRepository.findById(roomId).orElseThrow(() -> new OurException("Room Not Found"));
+            if (roomType != null) room.setRoomType(roomType);
+            if (roomPrice != null) room.setRoomPrice(roomPrice);
+            if (description != null) room.setRoomDescription(description);
+            if (imageUrl != null) room.setRoomPhotoUrl(imageUrl);
+
+            Room updatedRoom = roomRepository.save(room);
+            RoomDTO roomDTO = Utils.mapRoomEntityToRoomDTO(updatedRoom);
+
+            response.setStatusCode(200);
+            response.setMessage("successful");
+            response.setRoom(roomDTO);
+
+        } catch (OurException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error saving a room " + e.getMessage());
+        }
+        return response;
     }
 
     @Override
