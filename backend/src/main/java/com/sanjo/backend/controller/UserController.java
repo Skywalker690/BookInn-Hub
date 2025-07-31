3031b36624770c9a1ffe99e8838aad8d412954ce
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-31 15:31:10 +0530

    feat: add endpoint to fetch logged-in user's profile information

--- backend/src/main/java/com/sanjo/backend/controller/UserController.java
+++ backend/src/main/java/com/sanjo/backend/controller/UserController.java
@@ -4,6 +4,8 @@ import com.sanjo.backend.dto.Response;
 import com.sanjo.backend.service.interfac.IUserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -34,6 +36,16 @@ public class UserController {
         Response response = userService.deleteUser(userId);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
+
+    @GetMapping("/get-logged-in-profile-info")
+    public ResponseEntity<Response> getLoggedInUserProfile(){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email=authentication.getName();
+        Response response = userService.getMyInfo(email);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
 
 
 }
