292b27097530f0436632c421605c9becd4f33171
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-31 16:05:09 +0530

    feat: add admin-only endpoint to create a new room with validation

--- backend/src/main/java/com/sanjo/backend/controller/RoomController.java
+++ backend/src/main/java/com/sanjo/backend/controller/RoomController.java
@@ -1,4 +1,42 @@
 package com.sanjo.backend.controller;
 
+import com.sanjo.backend.dto.Response;
+import com.sanjo.backend.service.interfac.IBookingService;
+import com.sanjo.backend.service.interfac.IRoomService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/rooms")
 public class RoomController {
+
+    private final IRoomService roomService;
+    private final IBookingService iBookingService;
+
+    public RoomController(IRoomService roomService, IBookingService iBookingService) {
+        this.roomService = roomService;
+        this.iBookingService = iBookingService;
+    }
+
+    @PostMapping("/add")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> addNewRoom(
+            @RequestParam(value = "photo",required = false)MultipartFile photo,
+            @RequestParam(value = "roomType",required = false)String  roomType,
+            @RequestParam(value = "roomPrice",required = false) BigDecimal roomPrice,
+            @RequestParam(value = "roomDescription",required = false)String roomDescription
+            ){
+        if (photo == null || photo.isEmpty() || roomType == null || roomType.isBlank() || roomPrice == null || roomType.isBlank()) {
+            Response response = new Response();
+            response.setStatusCode(400);
+            response.setMessage("Please provide values for all fields(photo, roomType,roomPrice)");
+            return ResponseEntity.status(response.getStatusCode()).body(response);
+        }
+        Response response = roomService.addNewRoom(photo, roomType, roomPrice, roomDescription);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
 }
