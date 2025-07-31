c81ad72409af503ccd58fa3db4f08a672cbf39d2
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-31 17:05:59 +0530

    feat: add endpoint to fetch available rooms by date and type with request validation

--- backend/src/main/java/com/sanjo/backend/controller/RoomController.java
+++ backend/src/main/java/com/sanjo/backend/controller/RoomController.java
@@ -3,12 +3,14 @@ package com.sanjo.backend.controller;
 import com.sanjo.backend.dto.Response;
 import com.sanjo.backend.service.interfac.IBookingService;
 import com.sanjo.backend.service.interfac.IRoomService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -61,6 +63,22 @@ public class RoomController {
     @GetMapping("/all-available-rooms")
     public ResponseEntity<Response> getAvailableRooms() {
         Response response = roomService.getAllAvailableRooms();
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @GetMapping("/available-rooms-by-date-and-type")
+    public ResponseEntity<Response> getAvailableRoomsByDateAndType(
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate checkInDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate checkOutDate,
+            @RequestParam(required = false) String roomType
+    ) {
+        if (checkInDate == null || roomType == null || roomType.isBlank() || checkOutDate == null) {
+            Response response = new Response();
+            response.setStatusCode(400);
+            response.setMessage("Please provide values for all fields(checkInDate, roomType,checkOutDate)");
+            return ResponseEntity.status(response.getStatusCode()).body(response);
+        }
+        Response response = roomService.getAvailableRoomsByDataAndType(checkInDate, checkOutDate, roomType);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 }
