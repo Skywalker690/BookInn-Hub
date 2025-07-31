48479e66bd95ecfb5cca8d4c3a812c41d41d04d5
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-31 09:45:45 +0530

    feat: implement addNewRoom with image upload to S3 and room data persistence

--- backend/src/main/java/com/sanjo/backend/service/implementation/RoomService.java
+++ backend/src/main/java/com/sanjo/backend/service/implementation/RoomService.java
@@ -2,7 +2,17 @@ package com.sanjo.backend.service.implementation;
 
 
 import com.sanjo.backend.dto.Response;
+import com.sanjo.backend.dto.RoomDTO;
+import com.sanjo.backend.dto.UserDTO;
+import com.sanjo.backend.entity.Room;
+import com.sanjo.backend.entity.User;
+import com.sanjo.backend.exception.OurException;
+import com.sanjo.backend.repository.BookingRepository;
+import com.sanjo.backend.repository.RoomRepository;
+import com.sanjo.backend.service.AwsS3Service;
 import com.sanjo.backend.service.interfac.IRoomService;
+import com.sanjo.backend.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import java.math.BigDecimal;
@@ -11,9 +21,44 @@ import java.util.List;
 
 @Service
 public class RoomService implements IRoomService {
+
+    private final RoomRepository roomRepository;
+    private final BookingRepository bookingRepository;
+    private final AwsS3Service awsS3Service;
+
+    public RoomService(RoomRepository roomRepository, BookingRepository bookingRepository, AwsS3Service awsS3Service) {
+        this.roomRepository = roomRepository;
+        this.bookingRepository = bookingRepository;
+        this.awsS3Service = awsS3Service;
+    }
+
     @Override
     public Response addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice, String description) {
-        return null;
+        Response response = new Response();
+        try {
+            Room room = new Room();
+            String imageUrl =awsS3Service.saveImageToS3(photo);
+            room.setRoomPhotoUrl(imageUrl);
+            room.setRoomType(roomType);
+            room.setRoomDescription(description);
+            room.setRoomPrice(roomPrice);
+
+            Room savedRoom =roomRepository.save(room);
+            RoomDTO dto =Utils.mapRoomEntityToRoomDTO(savedRoom);
+
+            response.setStatusCode(200);
+            response.setMessage("Successful");
+
+
+        }catch (OurException e) {
+            response.setStatusCode(400);
+            response.setMessage(e.getMessage());
+        }
+        catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error Occurred While Adding Room "+e.getMessage());
+        }
+        return response;
     }
 
     @Override
