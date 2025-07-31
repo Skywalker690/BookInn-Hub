0caca9148deb8d5c4a34fd0702dc127e16b9e7e0
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-31 07:11:15 +0530

    feat: add getUserById functionality with DTO mapping and error handling

--- backend/src/main/java/com/sanjo/backend/service/implementation/UserService.java
+++ backend/src/main/java/com/sanjo/backend/service/implementation/UserService.java
@@ -164,7 +164,25 @@ public class UserService implements IUserService {
 
     @Override
     public Response getUserById(String userId) {
-        return null;
+        Response response = new Response();
+        try {
+            User user =userRepository.findById(Long.valueOf(userId)).orElseThrow(
+                    () -> new OurException("User not Found"));
+            UserDTO dto =Utils.mapUserEntityToUserDTO(user);
+
+            response.setUser(dto);
+            response.setStatusCode(200);
+            response.setMessage("Successful");
+
+        }catch (OurException e) {
+            response.setStatusCode(400);
+            response.setMessage(e.getMessage());
+        }
+        catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error Occurred While Getting Deleting User "+e.getMessage());
+        }
+        return response;
     }
 
     @Override
