a5838a11e1a60d54d5a35ca57298f323d1953d4d
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-31 15:04:55 +0530

    feat: add admin-only endpoint to fetch all users

--- backend/src/main/java/com/sanjo/backend/controller/UserController.java
+++ backend/src/main/java/com/sanjo/backend/controller/UserController.java
@@ -1,4 +1,29 @@
 package com.sanjo.backend.controller;
 
+import com.sanjo.backend.dto.Response;
+import com.sanjo.backend.service.interfac.IUserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final IUserService userService;
+
+    public UserController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> getAllUsers(){
+        Response response = userService.getAllUsers();
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+
 }
