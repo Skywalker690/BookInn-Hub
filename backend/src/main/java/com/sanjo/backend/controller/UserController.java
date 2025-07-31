5f86b4a81fdd197e736c373157f6baa3d5b26733
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-31 15:21:32 +0530

    feat: add endpoint to delete user by ID

--- backend/src/main/java/com/sanjo/backend/controller/UserController.java
+++ backend/src/main/java/com/sanjo/backend/controller/UserController.java
@@ -4,10 +4,7 @@ import com.sanjo.backend.dto.Response;
 import com.sanjo.backend.service.interfac.IUserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -29,6 +26,12 @@ public class UserController {
     @GetMapping("/get-by-id/{userId}")
     public ResponseEntity<Response> getAllUserById(@PathVariable("userId") String userId){
         Response response = userService.getUserById(userId);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @DeleteMapping("/get-by-id/{userId}")
+    public ResponseEntity<Response> deleteUser(@PathVariable("userId") String userId){
+        Response response = userService.deleteUser(userId);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
