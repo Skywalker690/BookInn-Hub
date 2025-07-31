2bbaea4e17c0f0e411b2e67d1b53e8362749becf
Skywalker690 <sanjoksiby0@gmail.com>
2025-07-31 07:49:39 +0530

    Implemented All methods from IRoomService

--- backend/src/main/java/com/sanjo/backend/service/implementation/RoomService.java
+++ backend/src/main/java/com/sanjo/backend/service/implementation/RoomService.java
@@ -1,4 +1,53 @@
 package com.sanjo.backend.service.implementation;
 
-public class RoomService {
+
+import com.sanjo.backend.dto.Response;
+import com.sanjo.backend.service.interfac.IRoomService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class RoomService implements IRoomService {
+    @Override
+    public Response addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice, String description) {
+        return null;
+    }
+
+    @Override
+    public List<String> getAllRoomTypes() {
+        return List.of();
+    }
+
+    @Override
+    public Response getAllRooms() {
+        return null;
+    }
+
+    @Override
+    public Response deleteRoom(Long roomId) {
+        return null;
+    }
+
+    @Override
+    public Response updateRoom(Long roomId, String description, String roomType, BigDecimal roomPrice, MultipartFile photo) {
+        return null;
+    }
+
+    @Override
+    public Response getRoomById(Long roomId) {
+        return null;
+    }
+
+    @Override
+    public Response getAvailableRoomsByDataAndType(LocalDate checkInDate, LocalDate checkOutDate, String roomType) {
+        return null;
+    }
+
+    @Override
+    public Response getAllAvailableRooms() {
+        return null;
+    }
 }
